590b8ae9b0ef4a097fd67fc6fd74cddcdfa21a3b
Waleed <22619190+waleed-alfaifi@users.noreply.github.com>
2023-10-26 15:37:19 +0300

    logging & upload to docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,25 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="udacity-project-4:v1.0.0"
 
 # Step 2:  
-# Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+# login to docker
+docker login
+
+
+# Get username from terminal input
+echo "Enter your docker username:"
+read username
+
+# Tag and print
+docker tag udacity-project-4:latest $username/$dockerpath
+echo "Docker ID and Image: $username/$dockerpath"
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $username/$dockerpath
+
+
+
