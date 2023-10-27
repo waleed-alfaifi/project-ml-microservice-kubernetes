89a8ece3d3f4ef36dec8a35cf44c2e3a388284e4
Waleed <22619190+waleed-alfaifi@users.noreply.github.com>
2023-10-27 22:25:52 +0300

    kubernetes deploy

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="waleed83/udacity-project-4:v1.0.0"
 
 # Step 2
-# Run the Docker Hub container with kubernetes
+# Create a deployment with the image 
+kubectl create deployment udacity-project-4 --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
+# Wait for pod to be ready
+kubectl wait --for=condition=ready pod -l app=udacity-project-4 --timeout=90s
 
 # Step 4:
 # Forward the container port to a host
+
+# this is used for debugging
+kubectl port-forward deployment/udacity-project-4 8000:80
+
+# this is used in porduction
+# kubectl expose deployment udacity-project-4 --type=NodePort --port=8000 --target-port=80
 
